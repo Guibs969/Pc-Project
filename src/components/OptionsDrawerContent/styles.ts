d972a24ff7eff2f08a2c1d6318053287b0134888
Marcos Prado <marcos.lprado@sempreceub.com>
2025-04-30 15:15:02 -0300

    feat: adds icon/style to options bar

--- src/components/OptionsDrawerContent/styles.ts
+++ src/components/OptionsDrawerContent/styles.ts
@@ -1,13 +1,42 @@
 import { StyleSheet } from "react-native";
 
-
-
 export const styles = StyleSheet.create({
-
-  nameCard : {
-    padding: 20,
+  drawer: {
+    backgroundColor: '#F4F6F8',
   },
-  drawer : {
-    backgroundColor: '#FAFAFA'
+
+  nameCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#E0E0E0',
+    backgroundColor: '#fff',
+    borderRadius: 15,
+  },
+
+  nameIcon: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  userName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#2E2E2E',
+  },
+
+  userInfo: {
+    fontSize: 14,
+    color: '#555',
+  },
+
+  drawerItem: {
+    marginVertical: 0,
+  },
+
+  drawerItemLabel: {
+    fontSize: 16,
+    color: '#2E2E2E',
   },
 });
