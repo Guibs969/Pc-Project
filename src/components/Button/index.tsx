a8cd74984f95f340d42b4f2e2746775e9b54efcf
Marcos Prado <marcos.lprado@sempreceub.com>
2025-05-06 14:47:00 -0300

    feat: creates edit/delete button and their styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,15 +2,15 @@ import { View,  TouchableOpacity, Text } from "react-native";
 import { styles } from "./styles";
 
 type Props =  {
-  tittle: string;
+  title: string;
   onPreess: () => void;
 }
 
-export function Button({tittle, onPreess}:Props) {
+export function Button({title, onPreess}:Props) {
   return (
     <View >
       <TouchableOpacity style={styles.button}onPress={onPreess} >
-        <Text style={styles.buttonText}>{tittle}</Text>
+        <Text style={styles.buttonText}>{title}</Text>
       </TouchableOpacity>
      
     </View>
